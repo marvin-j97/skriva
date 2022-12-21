7ee2ed1c9d19ffef693119bd39ab63475b60379e
marvin-j97 <33938500+marvin-j97@users.noreply.github.com>
2022-12-21 16:19:15 +0100

    Update index.ts

--- packages/skriva-transport-rotate-file/src/index.ts
+++ packages/skriva-transport-rotate-file/src/index.ts
@@ -7,6 +7,7 @@ import zlib from "zlib";
 
 type Options<T, L extends LogLevels, B extends BasePacket> = {
   format: Formatter<T, L, B, string>;
+
   /**
    * Whether to gzip old log files (defaults to true)
    */
@@ -20,42 +21,52 @@ type Options<T, L extends LogLevels, B extends BasePacket> = {
    * Filename including full path used by the stream
    */
   filename: string;
+
   /**
    * How often to rotate. Options are 'daily', 'custom' and 'test'. 'test' rotates every minute. If frequency is set to none of the above, a YYYYMMDD string will be added to the end of the filename.
    */
   frequency?: string;
+
   /**
    * If set, it will log to STDOUT when it rotates files and name of log file. Default is false.
    */
   verbose?: boolean;
+
   /**
    * Max number of logs to keep. If not set, it won't remove past logs. It uses its own log audit file to keep track of the log files in a json format. It won't delete any file not contained in it. It can be a number of files or number of days. If using days, add 'd' as the suffix.
    */
   maxLogs: string;
+
   /**
    * Location to store the log audit file. If not set, it will be stored in the root of the application.
    */
   auditFile: string;
+
   /**
    * Max size of the file after which it will rotate. It can be combined with frequency or date format. The size units are 'k', 'm' and 'g'. Units need to directly follow a number e.g. 1g, 100m, 20k.
    */
   size: string;
+
   /**
    *`Format as used in moment.js http://momentjs.com/docs/#/displaying/format/. The result is used to replace the '%DATE%' placeholder in the filename. If using 'custom' frequency, it is used to trigger file rotation when the string representation changes.
    */
   dateFormat?: string;
+
   /**
    * File extension to be appended to the filename. This is useful when using size restrictions as the rotation adds a count (1,2,3,4,...) at the end of the filename when the required size is met.
    */
   extension?: string;
+
   /**
    * Create a tailable symlink to the current active log file. Defaults to false
    */
   createSymlink?: boolean;
+
   /**
    * Name to use when creating the symbolic link. Defaults to 'current.log'
    */
   symlinkName?: string;
+
   /**
    * Use specified hashing algorithm for audit. Defaults to 'md5'. Use 'sha256' for FIPS compliance.
    */
